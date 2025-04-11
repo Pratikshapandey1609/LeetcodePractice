8e57741023a446e7096dde24de08e2599094d0bf
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-04-11 23:45:05 +0530

    Time: 2 ms (97.47%), Space: 41.9 MB (47.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,41 +1,24 @@
 class Solution {
-   
-  public char getVal(char ch){
-    
-    switch (ch){
+    public boolean isValid(String s) {
         
-        case ']':return '['; 
-        case '}':return '{'; 
-        case ')':return '('; 
-        default : return '\0'; 
-    }
- } 
-  
- public boolean isValid(String s) {
-        String Opening ="({[ "; 
-        String close ="]})";
-      
-    Stack<Character>st= new Stack <>(); 
-      
- for(char ch :s.toCharArray()){
-       
-    if (Opening.indexOf(ch)!=-1)
-         st.push(ch); 
-       
-    else{
-      
-      if(st.size()==0)
-        return false; 
-      
-         char temp =st.pop(); 
-          if (getVal(ch)!=temp)
-            return false; 
-        
-        }
-      }
-      
-     return st.size()==0; 
-   
-   }
-} 
+        Stack<Character> stack = new Stack<Character>();
 
+        for(char c : s.toCharArray()){
+
+            if(c == '('){
+                stack.push(')');
+            }
+            else if(c == '{'){
+                stack.push('}');
+            }
+            else if(c == '['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop() != c ){
+              return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
