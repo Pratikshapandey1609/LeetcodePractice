1e530a0fa9d99a56b998bab0c57b3e2afe20e8d9
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-05-21 09:42:24 +0530

    Time: 1 ms (72.83%) | Memory: 45.6 MB (82.97%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,52 @@
 class Solution {
         public void setZeroes(int[][] matrix) {
 
-        int n = matrix.length;
-        int m = matrix[0].length;
+        // int n = matrix.length;
+        // int m = matrix[0].length;
 
-        boolean [] row = new boolean [n];
-        boolean [] col = new boolean [m];
+        // boolean [] row = new boolean [n];
+        // boolean [] col = new boolean [m];
 
-        for(int i = 0; i  < n; i++){
-            for(int j = 0; j < m; j++){
+        // for(int i = 0; i  < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         if(matrix[i][j] == 0){
+        //             row[i] = true;
+        //             col[j] = true;
+        //         }
+        //     }
+        // }
+        
+        // for(int i = 0; i  < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         if(row[i] || col[j]){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+        
+        int  n = matrix.length;
+        int  m = matrix[0].length;
+
+        boolean []  row = new boolean[n];
+        boolean []  col = new boolean[m];
+
+        // check if mat[i][j] == 0 set it to true
+        for(int i = 0; i < n; i++){
+            for(int j  = 0; j < m; j++){
                 if(matrix[i][j] == 0){
                     row[i] = true;
                     col[j] = true;
                 }
             }
-        }
-        
-        for(int i = 0; i  < n; i++){
+        } 
+
+        // and again if row[i] || col[j] id true set it as 0;
+        for(int  i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(row[i] || col[j]){
                     matrix[i][j] = 0;
                 }
             }
         }
-
     }
 }
