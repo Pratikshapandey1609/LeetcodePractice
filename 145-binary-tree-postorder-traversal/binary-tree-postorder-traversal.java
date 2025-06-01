6f4ad4eff3ce915032a0a52db918d965fd123e3b
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-06-01 11:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (8.54%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,22 +15,47 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+    // //Recursive Approach here 
+    //     List<Integer>  res = new LinkedList<>();
+    //     postOrderTraversal(root , res);
+    //     return res;
+    // }
+    // //Recursive Approach here 
+    // public void postOrderTraversal(TreeNode root ,  List<Integer> res ){
         
-        List<Integer>  res = new LinkedList<>();
-        postOrderTraversal(root , res);
+    //     if(root == null){
+    //         return;
+    //     }
+    //     // left child
+    //     postOrderTraversal(root.left , res);
+    //     // right child
+    //     postOrderTraversal(root.right , res);
+    //     // root 
+    //     res.add(root.val);
+    
+    // Iterative Approach here
+     List<Integer>  res = new  ArrayList<Integer>();
+     Stack<TreeNode> stack = new Stack<>();
+     TreeNode node = root;
 
-        return res;
-    }
-    public void postOrderTraversal(TreeNode root ,  List<Integer> res ){
-        
-        if(root == null){
-            return;
-        }
-        // left child
-        postOrderTraversal(root.left , res);
-        // right child
-        postOrderTraversal(root.right , res);
-        // root 
-        res.add(root.val);
+      while(node != null || !stack.isEmpty()){
+        // move to left 
+          while(node != null){
+             stack.push(node);
+             node = node.left;
+            }
+        //move to right 
+           if(stack.peek().right != null){
+            node = stack.peek().right;
+           }else{
+             TreeNode temp = stack.pop();
+             res.add(temp.val);
+             while(!stack.isEmpty() && temp == stack.peek().right){
+                 temp = stack.pop();
+                 res.add(temp.val); 
+             }
+           }
+       }
+       return res;
     }
 }
