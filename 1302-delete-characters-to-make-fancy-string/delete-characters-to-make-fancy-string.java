2ff8df51994239cf78fbbd29d6c3e99369519895
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-07-21 18:26:02 +0530

    Time: 31 ms (79.29%) | Memory: 45.9 MB (40.66%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String makeFancyString(String s) {
+        // ------------------ Using extra space so complexity will be  O(n)
+        //------------------- Time complexity is also O(n)
+
+        // prev points to first character 
+        int prev = s.charAt(0);
+        int freq = 1;
+
+        StringBuilder ans = new StringBuilder();
+
+        // append first character in new string
+        ans.append(s.charAt(0));
+
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == prev){
+                freq++;
+            }else{
+                prev = s.charAt(i);
+                freq = 1;
+            }
+            if(freq < 3) ans.append(s.charAt(i));
+        }
+        return ans.toString();
+    }
+}
