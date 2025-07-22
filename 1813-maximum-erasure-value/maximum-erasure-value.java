71071907cbe06c66f4a4c2150eb870122c7932f0
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-07-22 17:58:43 +0530

    Time: 5 ms (99.47%) | Memory: 58.8 MB (74.55%) - LeetSync

---
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        
+        int n = nums.length;
+        boolean [] seen = new boolean[10001];
+        int left = 0,right = 0;
+        int curSum = 0;
+        int maxSum = 0;
+
+        for(right = 0; right < nums.length; right++){
+            while(seen[nums[right]]){
+                curSum -= nums[left];
+                seen[nums[left]] = false;
+                ++left;
+            }
+
+            curSum += nums[right];
+            seen[nums[right]] = true;
+            if(curSum > maxSum){
+                maxSum = curSum;
+            }
+        }
+        return maxSum;
+    }
+}
