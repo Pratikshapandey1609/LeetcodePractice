90d194cfd116bff3028f06befe7d63b80cd5ed82
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-07-27 19:37:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (41.19%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+
+    //     int count = 0;
+    //     for(int i = 1; i < nums.length-1; i++){
+
+    //         if(nums[i] == nums[i-1]) continue;
+
+    //         int left = i - 1 ;
+    //         int right = i + 1;
+
+    //         // Find right different value
+    //         while(left >= 0 && nums[left] == nums[i]){
+    //             left--;
+    //         }
+            
+    //         // Find right different value
+    //         while(right < nums.length && nums[right] == nums[i]){
+    //             right--;
+    //         }
+    //         if(left >= 0 && right < nums.length){
+    //             if(nums[i] > nums[left] && nums[i] > nums[right]){
+    //                 count++;
+    //             }else if(nums[i] < nums[left] && nums[i] < nums[right]){
+    //                count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    int count = 0;
+    int left = 0;
+    // int right = nums[i] + 1;
+    for(int i = 1; i < nums.length - 1; i++){
+        if(nums[i] != nums[i + 1]){
+            if((nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+              (nums[i] < nums[left] && nums[i] < nums[i + 1])){
+               count++;
+            }
+
+            left = i;
+        }
+    }
+    return count;
+    }
+}
