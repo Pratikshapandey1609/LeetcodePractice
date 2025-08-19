027305d9645aa670a972e3b9a6a8c6cf21063d33
pratiksha pandey <139357332+Pratikshapandey1609@users.noreply.github.com>
2025-08-19 23:57:52 +0530

    Time: 6 ms (86.26%) | Memory: 43.1 MB (68.66%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,18 +1,3 @@
-// class Solution {
-//     public String reverseWords(String s) {
-
-//         String [] words  = s.split("'\\s+'");
-//         StringBuilder res = new StringBuilder();
-
-//         for(int i = words.length - 1; i >= 0; i--){
-//             res.append(words[i]);
-//             if(i != 0){
-//                 res.append(" ");
-//             }
-//         }
-//         return res.toString().trim();
-//     }
-// }
 class Solution {
     public String reverseWords(String s) {
         String[] words = s.split("\\s+");
